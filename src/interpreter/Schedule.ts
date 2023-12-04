4817644e8fda522b51c88810248c9971d55df052
Anis Megdiche <anis.megdiche@gmail.com>
2023-12-04 19:02:04 +0100

    update schedule feature

--- src/interpreter/Schedule.ts
+++ src/interpreter/Schedule.ts
@@ -13,6 +13,11 @@ import { Logger } from '../lib/Logger'
 import { Config } from '../server/Config'
 import { Plans } from './Plans'
 
+type TScheduleConfig = {
+    plan: string
+    entity: string
+    cron: string
+}
 
 export abstract class Schedule {
 
@@ -21,30 +26,26 @@ export abstract class Schedule {
     public static CreateAndStart() {
         Logger.Debug(`${Logger.In} Schedule.Start: ${JSON.stringify(Config.Configuration.schedules)}`)
         if (Config.Configuration?.schedules) {
-            for (const _scheduleConfig of Config.Configuration.schedules) {
-                const __schedule = Object.keys(_scheduleConfig)[0]
-                const
-                    __jobName = __schedule,
-                    __planName = _scheduleConfig[__schedule].plan,
-                    __entityName = _scheduleConfig[__schedule].entity,
-                    __cron = _scheduleConfig[__schedule].cron
 
-                Logger.Info(`${Logger.In} Schedule.Start: Starting job '${__jobName}'`)
+            const _configSchedule: Array<[string, TScheduleConfig]> = Object.entries(Config.Configuration.schedules)
+
+            for (const [_scheduleName, _scheduleConfig] of _configSchedule) {
+                Logger.Info(`${Logger.In} Schedule.Start: Starting job '${_scheduleName}'`)
                 this.Jobs.push(<TSchedule>{
-                    name: __jobName,
+                    name: _scheduleName,
                     job: new CronJob(
-                        __cron,
+                        _scheduleConfig.cron,
                         () => {
-                            Logger.Debug(`${Logger.In} Schedule.Start: Running job '${__jobName}'`)
+                            Logger.Debug(`${Logger.In} Schedule.Start: Running job '${_scheduleName}'`)
                             try {
-                                Plans.RenderTable(undefined, __planName, __entityName)
+                                Plans.RenderTable(undefined, _scheduleConfig.plan, _scheduleConfig.entity)
                             } catch (error) {
-                                Logger.Error(`${Logger.In} Schedule.Start: Error has occured with '${__jobName}' : ${JSON.stringify(error)}`)
+                                Logger.Error(`${Logger.In} Schedule.Start: Error has occured with '${_scheduleName}' : ${JSON.stringify(error)}`)
                             }
                         },
                         null,
                         true,
-                        Config.Configuration.server.timezone || 'UTC'
+                        Config.Configuration?.server?.timezone || 'UTC'
                     )
                 })
             }
