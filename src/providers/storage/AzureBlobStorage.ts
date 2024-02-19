ed48450d14dc78ccc531eda6a9d18d50b0ef8a99
Anis Megdiche <anis.megdiche@gmail.com>
2024-02-19 15:44:32 +0100

    Add configuration and initialization logic for Azure Blob Storage.

--- src/providers/storage/AzureBlobStorage.ts
+++ src/providers/storage/AzureBlobStorage.ts
@@ -6,33 +6,61 @@
 import { BlobServiceClient, ContainerClient } from '@azure/storage-blob'
 //
 import { CommonStorage, IStorage } from "./CommonStorage"
+import { Logger } from '../../lib/Logger'
 
+type TAzureBlobStorageConfig = {
+    connectionString?: string
+    containerName?: string
+    createContainerIfNotExists?: boolean
+}
 
 export class AzureBlobStorage extends CommonStorage implements IStorage {
 
     #BlobServiceClient: BlobServiceClient | undefined
     #ContainerClient: ContainerClient | undefined
 
+    Config: TAzureBlobStorageConfig = {}
+
+    Init(): void {
+        Logger.Debug("AzureBlobStorage.Init")
+        this.Config = <TAzureBlobStorageConfig>{
+            connectionString: this.Params.host,
+            containerName: this.Params.database,
+            createContainerIfNotExists: this.Options.azureBlobCreateContainerIfNotExists || false
+        }
+    }
+
     async Connect(): Promise<void> {
-        const { azureBlobConnectionString: connectionString, azureBlobContainerName: containerName } = this.Options
+        const { connectionString, containerName } = this.Config
 
         if (!connectionString || !containerName) {
-            throw new Error('Missing Azure Blob Storage connection string or container name')
+            Logger.Error('AzureBlobStorage: Missing Azure Blob Storage connection string or container name')
+            this.Disconnect()
+            return
         }
 
         this.#BlobServiceClient = BlobServiceClient.fromConnectionString(connectionString)
         this.#ContainerClient = this.#BlobServiceClient.getContainerClient(containerName)
+        await this.#ContainerClient.createIfNotExists()
+
     }
 
     async Disconnect(): Promise<void> {
-        // You may close connections or release resources here
         this.#BlobServiceClient = undefined
         this.#ContainerClient = undefined
     }
 
+    async IsConnected(): Promise<boolean> {
+        if (!this.#ContainerClient) {
+            Logger.Error('AzureBlobStorage: Connection to Azure Blob Storage not established')
+            return false
+        }
+        return true
+    }
+
     async IsExist(file: string): Promise<boolean> {
         if (!this.#ContainerClient) {
-            throw new Error('Connection to Azure Blob Storage not established')
+            throw new Error('AzureBlobStorage: Connection to Azure Blob Storage not established')
         }
 
         const blobClient = this.#ContainerClient.getBlockBlobClient(file)
