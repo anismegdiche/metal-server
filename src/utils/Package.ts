cf7c3f40b964e17fbf1f1a08112bc1364ae982e0
Anis Megdiche <anis.megdiche@gmail.com>
2024-11-04 15:27:55 +0100

    "Added 'readonly' keyword to 'Json' property in Package class"

--- src/utils/Package.ts
+++ src/utils/Package.ts
@@ -10,5 +10,5 @@ import { TJson } from "../types/TJson"
 const packageRaw = Fs.readFileSync('./package.json', 'utf8')
 
 export class Package {
-    static Json : TJson = JSON.parse(packageRaw) 
+    static readonly Json : TJson = JSON.parse(packageRaw) 
 }
