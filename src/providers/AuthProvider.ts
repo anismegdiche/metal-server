89923bfc14c094c2460bae0ca0b33cd5543542db
Anis Megdiche <anis.megdiche@gmail.com>
2024-11-09 12:56:27 +0100

    "Removed unnecessary `this` references in AuthProvider class"

--- src/providers/AuthProvider.ts
+++ src/providers/AuthProvider.ts
@@ -20,20 +20,19 @@ export class AuthProvider {
     static readonly #AuthFactory = new Factory<ACAuthProvider>()
     static Provider: ACAuthProvider
 
-
     static GetProvider(providerName: string): ACAuthProvider {
-        if (this.#AuthFactory.Has(providerName))
-            return this.#AuthFactory.Get(providerName)!
+        if (AuthProvider.#AuthFactory.Has(providerName))
+            return AuthProvider.#AuthFactory.Get(providerName)!
         else
             throw new HttpErrorNotFound(`Provider '${providerName}' not found`)
     }
 
     static RegisterProviders() {
-        this.#AuthFactory.Register(AUTH_PROVIDER.DEMO, new DemoAuthProvider())
-        this.#AuthFactory.Register(AUTH_PROVIDER.BASIC, new BasicAuthProvider())
+        AuthProvider.#AuthFactory.Register(AUTH_PROVIDER.DEMO, new DemoAuthProvider())
+        AuthProvider.#AuthFactory.Register(AUTH_PROVIDER.BASIC, new BasicAuthProvider())
     }
 
     static SetCurrent(providerName: string) {
-        this.Provider = this.GetProvider(providerName)
+        AuthProvider.Provider = AuthProvider.GetProvider(providerName)
     }
 }
