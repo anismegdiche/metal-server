ffcb8cc371c234f88b88405d5f3366b646982219
Anis Megdiche <anis.megdiche@gmail.com>
2025-01-30 14:32:15 +0100

    refactor: improve EvaluateJsCode methods for better handling of JavaScript code and context variables

--- src/utils/PlaceHolder.ts
+++ src/utils/PlaceHolder.ts
@@ -3,12 +3,17 @@
 //
 //
 //
-
-import { RX } from "../lib/Const"
 import { JsonHelper } from "../lib/JsonHelper"
 import { StringHelper } from "../lib/StringHelper"
 import { Sandbox } from "../server/Sandbox"
 import { Logger } from "./Logger"
+import { TJson } from "../types/TJson"
+
+
+//
+// const RX_JS_CODE: RegExp = /\$\{\{([^}]+)}\}/m
+const RX_JS_CODE: RegExp = /\$\{\{(.*?)\}\}/m
+const RX_CONTEXT_VAR: RegExp = /\$[\w.]+/g
 
 
 //
@@ -18,36 +23,76 @@ export class PlaceHolder {
         if (StringHelper.IsEmpty(str))
             return undefined
 
-        const matches = str.match(RX.CONTEXT_VAR)
+        const matches = str.match(RX_CONTEXT_VAR)
         return matches ?? undefined
     }
 
-
-    static EvaluateJsCode<T = string>(stringWithJsCode: T | undefined, sandBox: Sandbox): T | undefined {
-        if (stringWithJsCode === undefined)
+    static EvaluateJsCode<T>(jsCode: any, sandBox: Sandbox): T | undefined {
+        // case undefined
+        if (jsCode === undefined || jsCode === null)
             return undefined
 
-        const isString = typeof stringWithJsCode === 'string'
+        // case no js code
+        const _jsCodeString = (typeof jsCode === 'string')
+            ? jsCode
+            : JsonHelper.Stringify(jsCode)
 
-        const _stringWithJsCode = isString
-            ? stringWithJsCode
-            : JsonHelper.Stringify(stringWithJsCode)
+        if (RX_JS_CODE.exec(_jsCodeString) === null)
+            return jsCode
 
-        const evaluated = _stringWithJsCode.replace(RX.JS_CODE, (_match, _code) => {
+        // case js code type
+        switch (true) {
+            case Array.isArray(jsCode):
+                return jsCode.map(item => PlaceHolder.EvaluateJsCode(item, sandBox)) as T
+
+            case typeof jsCode === 'object' && !Array.isArray(jsCode):
+                return PlaceHolder.EvaluateJsCodeObject(jsCode, sandBox) as T
+
+            case typeof jsCode === 'string':
+                return (jsCode.trim().startsWith('${{') && jsCode.trim().endsWith('}}'))
+                    ? PlaceHolder.EvaluateJsCodeStringObject(jsCode, sandBox) as T
+                    : PlaceHolder.EvaluateJsCodeString(jsCode, sandBox) as T
+
+            default:
+                return jsCode
+        }
+    }
+
+    static EvaluateJsCodeString(jsCode: string, sandBox: Sandbox): string {
+        return jsCode.replace(RX_JS_CODE, (_match, _code) => {
             try {
-                const __result = sandBox.Evaluate(_code)
-                return (__result === undefined)
-                    ? ''
-                    : __result.toString()
+                // file deepcode ignore ImproperCodeSanitization: // TODO Review this
+                const result = sandBox.Evaluate(_code)
+                if (result === undefined)
+                    throw new Error(`PlaceHolder.EvaluateJsCodeString_v2: Failed to evaluate code: ${_code}`)
+                return StringHelper.ToString(result)
             } catch (error: unknown) {
                 Logger.Error(error)
-                // Return the original placeholder if there's an error
-                return _match
+                return _match // Preserve original placeholder if an error occurs
             }
         })
+    }
 
-        return isString
-            ? evaluated as T | undefined
-            : JsonHelper.TryParse<T | undefined>(evaluated, undefined)
+    static EvaluateJsCodeStringObject(jsCode: string, sandBox: Sandbox): any | undefined {
+        const match = RX_JS_CODE.exec(jsCode)
+
+        if (match?.[1])
+            return sandBox.Evaluate(match[1].trim())        
+
+        return undefined
+    }
+
+    static EvaluateJsCodeObject(jsCode: TJson, sandBox: Sandbox): TJson {
+        const result: Record<string, unknown> = {}
+        for (const key in jsCode) {
+            if (Object.hasOwn(jsCode, key)) {
+                const value = jsCode[key] as string | TJson | TJson[]
+                const evaluatedKey = PlaceHolder.EvaluateJsCode<string>(key, sandBox)
+                const evaluatedValue = PlaceHolder.EvaluateJsCode(value, sandBox)
+                if (evaluatedKey && evaluatedValue)
+                    result[evaluatedKey] = evaluatedValue
+            }
+        }
+        return result
     }
 }
