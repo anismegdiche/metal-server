a4d78a20a0a545ae946e5dd02d33fc6368c2634d
Anis Megdiche <anis.megdiche@gmail.com>
2025-02-03 16:29:50 +0100

    refactor: remove commented-out code in TContext type definition

--- src/@types/TContext.ts
+++ src/@types/TContext.ts
@@ -10,13 +10,10 @@ import { TJson } from "../types/TJson"
 export type TContext = {
     $entity?: string,           // requested entity name
     $schema?: string,           // requested schema name
-    // available for schema request
     $request?: {
         "data-path"?: string    // requested JSON path, if undefined will return the whole JSON
     },
-    // available for webservice config (update,delete)
     $item?: TJson
-    // available when connected to a webservice
     $response?: {
         url?: string,
         host?: string,
