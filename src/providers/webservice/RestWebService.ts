95282c314af9a1ca7913dce13ec6574e771c0610
Anis Megdiche <anis.megdiche@gmail.com>
2025-02-03 15:21:45 +0100

    Rest service refactor

--- src/providers/webservice/RestWebService.ts
+++ src/providers/webservice/RestWebService.ts
@@ -3,10 +3,11 @@
 //
 //
 //
+import _ from "lodash"
 import { Readable } from "stream"
 import axios, { AxiosResponse, AxiosInstance } from "axios"
 //
-import { absWebServiceProvider, ENDPOINT, HEADER, TWebServiceEndpointMethodConfig } from '../absWebServiceProvider'
+import { absWebServiceProvider, ENDPOINT, HEADER } from '../absWebServiceProvider'
 import { TConfigSourceWebService, TWebServiceDataOptions } from "../data/WebServiceData"
 import { Logger } from "../../utils/Logger"
 import { StringHelper } from "../../lib/StringHelper"
@@ -14,13 +15,16 @@ import { HttpErrorInternalServerError, HttpErrorSwitch } from "../../server/Http
 import { JsonHelper } from '../../lib/JsonHelper'
 import { PlaceHolder } from "../../utils/PlaceHolder"
 import { Sandbox } from "../../server/Sandbox"
+import { TContext } from "../../@types/TContext"
+import { CONTENT } from "../ContentProvider"
+import { TJson } from "../../types/TJson"
 
 
 //
 export class RestWebService extends absWebServiceProvider {
 
     DEFAULT: Partial<TWebServiceDataOptions> = {
-        endpoints: {}
+        content: CONTENT.JSON
     }
 
     ConfigSource?: TConfigSourceWebService
@@ -35,75 +39,87 @@ export class RestWebService extends absWebServiceProvider {
 
     SetConfig(configSource: TConfigSourceWebService): void {
         super.SetConfig(configSource)
-
-        if (this.ConfigSourceOptions?.endpoints === undefined || this.ConfigSource?.host === undefined)
-            throw new HttpErrorInternalServerError('RestWebService: No urls defined in config')
-
-        this.ProcessEndpoints('collection', this.ConfigSourceOptions.endpoints.collection)
-        this.ProcessEndpoints('item', this.ConfigSourceOptions.endpoints.item)
     }
 
     @Logger.LogFunction()
-    Init(): void {
-        if (!this.ConfigSourceOptions?.content)
-            return
+    async Init(): Promise<void> {
 
         this.Client = axios.create()
 
         this.Client.defaults.baseURL = this.ConfigSource!.host
 
         // set content type
-        const [header] = Object.keys(HEADER[this.ConfigSourceOptions.content])
-        const [value] = Object.values(HEADER[this.ConfigSourceOptions.content])
+        const [header] = Object.keys(HEADER[this.ConfigSourceOptions!.content])
+        const [value] = Object.values(HEADER[this.ConfigSourceOptions!.content])
 
         if (typeof header == 'string' && typeof value == 'string')
             this.Client.defaults.headers.common[header] = value
     }
 
+    async RequestClient(endpointType: ENDPOINT, httpStatusSuccess: number[], data?: TJson, $context?: Partial<TContext>): Promise<Readable> {
+
+        if (!this.Endpoints.has(endpointType) || !this.Client)
+            throw new HttpErrorInternalServerError(`${endpointType}: undefined endpoint for ${endpointType}`)
+
+        try {
+            const { Method, Url, Data, SessionHeaders, DataPath } = this.Endpoints.get(endpointType)!
+
+            const _Method = PlaceHolder.EvaluateJsCode<string>(Method, new Sandbox($context))
+            const _Url = PlaceHolder.EvaluateJsCode<string>(
+                StringHelper.Url(
+                    $context?.$entity,
+                    Url
+                ),
+                new Sandbox($context)
+            )
+            const _Data = PlaceHolder.EvaluateJsCode(Data, new Sandbox($context))
+            const _DataPath = PlaceHolder.EvaluateJsCode(DataPath, new Sandbox($context))
+
+            Logger.Debug(`${Logger.In} ${endpointType}: ${StringHelper.Url(this.ConfigSource!.host, Url)}`)
+
+            const wsResp: AxiosResponse = await this.Client({
+                method: (_Method ?? Method).toLowerCase(),
+                url: _Url,
+                data: _Data ?? JsonHelper.Stringify(data)
+            })
+
+            if (!httpStatusSuccess.includes(wsResp.status))
+                throw HttpErrorSwitch(wsResp.status, `${endpointType}: ${wsResp.statusText}`)
+
+            // eslint-disable-next-line no-param-reassign
+            $context = _.merge(
+                $context,
+                {
+                    $request: {
+                        "data-path": _DataPath
+                    },
+                    $response: {
+                        url: wsResp.config.url,
+                        host: wsResp.request.host,
+                        body: wsResp.data
+                    }
+                }
+            )
+
+            // set session headers after request
+            if (SessionHeaders) {
+                const _SessionHeaders = PlaceHolder.EvaluateJsCode(SessionHeaders, new Sandbox($context)) as Exclude<TJson<string>, undefined>
+                for (const [_headerName, _headerValue] of Object.entries(_SessionHeaders)) {
+                    this.Client.defaults.headers.common[_headerName] = _headerValue
+                }
+            }
+
+            return Readable.from(JsonHelper.Stringify(wsResp.data))
+
+        } catch (error: any) {
+            throw HttpErrorSwitch(error.status, error.message)
+        }
+    }
+
     @Logger.LogFunction()
     async Connect(): Promise<void> {
-        if (typeof this.ConfigSourceOptions?.endpoints.session !== 'object' || !this.Client)
-            return
-
-        const [loginEndpointMethod] = Object.keys(this.ConfigSourceOptions.endpoints.session)
-        if (!loginEndpointMethod)
-            return
-
-        const loginEndpointConfig: TWebServiceEndpointMethodConfig = this.ConfigSourceOptions.endpoints.session[loginEndpointMethod]
-
-        if (!loginEndpointConfig)
-            return
-
-        Logger.Debug(`${Logger.In} RestWebService.Connect: ${StringHelper.Url(this.ConfigSource!.host, JsonHelper.Stringify(loginEndpointConfig))}`)
-        const wsLogin = await this.Client({
-            method: loginEndpointMethod.toLowerCase(),
-            url: StringHelper.Url(
-                this.ConfigSource!.host,
-                loginEndpointConfig.request
-            ),
-            data: loginEndpointConfig.data
-        })
-
-        if (wsLogin.status !== 200)
-            throw new HttpErrorInternalServerError(`RestWebService.Connect: ${wsLogin.statusText}`)
-
-        if (!loginEndpointConfig["session-headers"])
-            return
-
-        const postHearders = loginEndpointConfig["session-headers"]
-
-        for (const [headerName, headerValue] of Object.entries(postHearders)) {
-            const __headerNewValue = PlaceHolder.EvaluateJsCode(
-                headerValue,
-                new Sandbox({
-                    $response: {
-                        url: wsLogin.config.url,
-                        host: wsLogin.request.host,
-                        body: wsLogin.data
-                    }
-                }))
-            this.Client.defaults.headers.common[headerName] = __headerNewValue
-        }
+        if (this.Endpoints.has(ENDPOINT.SESSION))
+            this.RequestClient(ENDPOINT.SESSION, [200])
     }
 
     // eslint-disable-next-line class-methods-use-this
@@ -113,103 +129,22 @@ export class RestWebService extends absWebServiceProvider {
     }
 
     @Logger.LogFunction()
-    async Create(endpoint: string, data: string): Promise<Readable> {
-
-        if (!this.Endpoints.has(ENDPOINT.ITEM_CREATE) || !this.Client)
-            throw new HttpErrorInternalServerError(`RestWebService.Create: undefined endpoint for ${ENDPOINT.ITEM_CREATE}`)
-
-        try {
-            const { Method } = this.Endpoints.get(ENDPOINT.ITEM_CREATE)!
-
-            Logger.Debug(`${Logger.In} RestWebService.Create: ${StringHelper.Url(this.ConfigSource!.host, endpoint)}`)
-            const wsResp: AxiosResponse = await this.Client({
-                method: Method.toLowerCase(),
-                url: endpoint,
-                data
-            })
-
-            if (![200, 201].includes(wsResp.status))
-                throw HttpErrorSwitch(wsResp.status, `RestWebService.Create: ${wsResp.statusText}`)
-
-            return Readable.from(JsonHelper.Stringify(wsResp.data))
-        } catch (error: any) {
-            throw HttpErrorSwitch(error.status, error.message)
-        }
+    async Create(data: TJson, $context: Partial<TContext>): Promise<Readable> {
+        return this.RequestClient(ENDPOINT.ITEM_CREATE, [200, 201], data, $context)
     }
 
     @Logger.LogFunction()
-    async Read(endpoint: string): Promise<Readable> {
-
-        if (!this.Endpoints.has(ENDPOINT.COLLECTION_READ) || !this.Client)
-            throw new HttpErrorInternalServerError(`RestWebService.Read: undefined endpoint for ${ENDPOINT.COLLECTION_READ}`)
-
-        try {
-            const { Method } = this.Endpoints.get(ENDPOINT.COLLECTION_READ)!
-
-            Logger.Debug(`${Logger.In} RestWebService.Read: ${StringHelper.Url(this.ConfigSource!.host, endpoint)}`)
-            const wsResp: AxiosResponse = await this.Client({
-                method: Method.toLowerCase(),
-                url: endpoint
-            })
-
-            if (wsResp.status !== 200)
-                throw HttpErrorSwitch(wsResp.status, `RestWebService.Read: ${wsResp.statusText}`)
-
-            return Readable.from(JsonHelper.Stringify(wsResp.data))
-
-        } catch (error: any) {
-            throw HttpErrorSwitch(error.status, error.message)
-        }
+    async Read($context: Partial<TContext>): Promise<Readable> {
+        return this.RequestClient(ENDPOINT.COLLECTION_READ, [200], undefined, $context)
     }
 
     @Logger.LogFunction()
-    async Update(endpoint: string, body: string): Promise<Readable> {
-
-        if (!this.Endpoints.has(ENDPOINT.ITEM_UPDATE) || !this.Client)
-            throw new HttpErrorInternalServerError(`RestWebService.Update: undefined endpoint for ${ENDPOINT.ITEM_UPDATE}`)
-
-        try {
-            const { Method } = this.Endpoints.get(ENDPOINT.ITEM_UPDATE)!
-
-            Logger.Debug(`${Logger.In} RestWebService.Update: ${StringHelper.Url(this.ConfigSource!.host, endpoint)}`)
-            const wsResp: AxiosResponse = await this.Client({
-                method: Method.toLowerCase(),
-                url: endpoint,
-                data: body
-            })
-
-            if (![200, 204].includes(wsResp.status))
-                throw HttpErrorSwitch(wsResp.status, `RestWebService.Update: ${wsResp.statusText}`)
-
-            return Readable.from(JsonHelper.Stringify(wsResp.data))
-
-        } catch (error: any) {
-            throw HttpErrorSwitch(error.status, error.message)
-        }
+    async Update(data: TJson, $context: Partial<TContext>): Promise<Readable> {
+        return this.RequestClient(ENDPOINT.ITEM_UPDATE, [200, 204], data, $context)
     }
 
     @Logger.LogFunction()
-    async Delete(endpoint: string): Promise<Readable> {
-
-        if (!this.Endpoints.has(ENDPOINT.ITEM_DELETE) || !this.Client)
-            throw new HttpErrorInternalServerError(`RestWebService.Delete: undefined endpoint for ${ENDPOINT.ITEM_DELETE}`)
-
-        try {
-            const { Method } = this.Endpoints.get(ENDPOINT.ITEM_DELETE)!
-
-            Logger.Debug(`${Logger.In} RestWebService.Delete: ${StringHelper.Url(this.ConfigSource!.host, endpoint)}`)
-            const wsResp: AxiosResponse = await this.Client({
-                method: Method.toLowerCase(),
-                url: endpoint
-            })
-
-            if (![200, 204].includes(wsResp.status))
-                throw HttpErrorSwitch(wsResp.status, `RestWebService.Delete: ${wsResp.statusText}`)
-
-            return Readable.from(JsonHelper.Stringify(wsResp.data))
-
-        } catch (error: any) {
-            throw HttpErrorSwitch(error.status, error.message)
-        }
+    async Delete($context: Partial<TContext>): Promise<Readable> {
+        return this.RequestClient(ENDPOINT.ITEM_DELETE, [200, 204], undefined, $context)
     }
 }
