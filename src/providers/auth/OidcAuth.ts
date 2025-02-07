5c47c63a004cd59584c39cdc32c839b356cf0e47
Anis Megdiche <anis.megdiche@gmail.com>
2025-02-07 16:05:55 +0100

    clean up

--- src/providers/auth/OidcAuth.ts
+++ src/providers/auth/OidcAuth.ts
@@ -50,7 +50,7 @@ export class OidcAuth extends absAuthProvider {
 
     @Logger.LogFunction()
     async Init(): Promise<void> {
-        //FIXME workaround for SSL/TLS errors
+        //TODO workaround for SSL/TLS errors
         // file deepcode ignore InsecureTLSConfig: Workround
         process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
