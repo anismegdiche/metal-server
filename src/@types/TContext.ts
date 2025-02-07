c6ba6d834208a595df06cffc5262860e787d3e10
Anis Megdiche <anis.megdiche@gmail.com>
2025-02-07 16:05:08 +0100

    feat: extend TContext type with additional options for data handling and filtering

--- src/@types/TContext.ts
+++ src/@types/TContext.ts
@@ -10,6 +10,17 @@ import { TJson } from "../types/TJson"
 export type TContext = {
     $entity?: string,           // requested entity name
     $schema?: string,           // requested schema name
+    $options: {
+        // from Config
+        anonymize?: string | string[]
+        // from Request Options
+        data?: TJson | TJson[]
+        fields?: string
+        filter?: TJson
+        "filter-expression"?: string
+        sort?: string
+        cache?: number
+    }
     $request?: {
         "data-path"?: string    // requested JSON path, if undefined will return the whole JSON
     },
