c1c95ec14781a74588bc043770c6ca0df889c5fa
Anis Megdiche <anis.megdiche@gmail.com>
2025-02-27 16:16:57 +0100

    fix: add check for 'plans' configuration before initializing Plans

--- src/server/Plans.ts
+++ src/server/Plans.ts
@@ -16,8 +16,11 @@ export class Plans {
     static readonly Plans = new Map<string, Plan>()
 
     static Init() {
+        if (!Config.Has('plans'))
+            return
+
         // eslint-disable-next-line you-dont-need-lodash-underscore/keys
-        const plans = _.keys(Config.Get<TJson>("plans", {}))
+        const plans = _.keys(Config.Get<TJson>("plans") ?? {})
 
         plans.forEach((plan: string) => {
             Plans.Plans.set(plan, new Plan(plan))
