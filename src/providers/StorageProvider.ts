037911f368199131cb721e0d290d08c85e6db90d
Anis Megdiche <anis.megdiche@gmail.com>
2025-04-11 20:24:56 +0200

    feat: add Azure Data Lake Storage support to StorageProvider

--- src/providers/StorageProvider.ts
+++ src/providers/StorageProvider.ts
@@ -8,6 +8,7 @@ import { Factory } from "../utils/Factory"
 import { absStorageProvider } from "./absStorageProvider"
 import { AmazonS3Storage, TAmazonS3StorageConfig } from "./storage/AmazonS3Storage"
 import { AzureBlobStorage, TAzureBlobStorageConfig } from "./storage/AzureBlobStorage"
+import { AzureDataLakeStorage, TAzureDataLakeStorageConfig } from "./storage/AzureDataLakeStorage"
 import { AzureFileStorage, TAzureFileStorageConfig } from "./storage/AzureFileStorage"
 import { FsStorage, TFsStorageConfig } from "./storage/FsStorage"
 import { FtpStorage, TFtpStorageConfig } from "./storage/FtpStorage"
@@ -21,6 +22,7 @@ export enum STORAGE {
     SMB = "smb",
     AZURE_BLOB = "az-blob",
     AZURE_FILE = "az-file",
+    AZURE_DATALAKE_G2 = "az-datalake",
     AMAZON_S3 = "s3"
 }
 
@@ -29,6 +31,7 @@ export type TStorageConfig = TFsStorageConfig
     & TSmbStorageConfig
     & TAzureBlobStorageConfig
     & TAzureFileStorageConfig
+    & TAzureDataLakeStorageConfig
     & TAmazonS3StorageConfig
 
 
@@ -46,10 +49,11 @@ export class StorageProvider {
 
     static RegisterProviders() {
         StorageProvider.#StorageFactory.Register(STORAGE.FILESYSTEM, new FsStorage())
-        StorageProvider.#StorageFactory.Register(STORAGE.AZURE_BLOB, new AzureBlobStorage())
         StorageProvider.#StorageFactory.Register(STORAGE.FTP, new FtpStorage())
         StorageProvider.#StorageFactory.Register(STORAGE.SMB, new SmbStorage())
+        StorageProvider.#StorageFactory.Register(STORAGE.AZURE_BLOB, new AzureBlobStorage())
         StorageProvider.#StorageFactory.Register(STORAGE.AZURE_FILE, new AzureFileStorage())
+        StorageProvider.#StorageFactory.Register(STORAGE.AZURE_DATALAKE_G2, new AzureDataLakeStorage())
         StorageProvider.#StorageFactory.Register(STORAGE.AMAZON_S3, new AmazonS3Storage())
     }
 }
