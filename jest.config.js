df5987903d907fad3797830db5e342e014916279
Anis Megdiche <anis.megdiche@gmail.com>
2025-06-23 17:07:35 +0200

    style: format jest.config.js for consistency

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: [
-    "**/__tests__/**/*.ts?(x)", 
-    "**/?(*.)+(spec|test).ts?(x)",
-    "**/client/__tests__/**/*.js"
-  ],
-  transform: {
-    '^.+.ts?$': ['ts-jest', { diagnostics: false }]
-  },
-  transformIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/build/'
-  ]
-};
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: [
+        "**/__tests__/**/*.ts?(x)",
+        "**/?(*.)+(spec|test).ts?(x)",
+        "**/client/__tests__/**/*.js"
+    ],
+    transform: {
+        '^.+.ts?$': ['ts-jest', { diagnostics: false }]
+    },
+    transformIgnorePatterns: [
+        '<rootDir>/node_modules/',
+        '<rootDir>/build/'
+    ]
+}
