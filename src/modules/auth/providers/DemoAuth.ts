25de9b2b4878dbd278de3a2befd6bec7bbbf74a3
Anis Megdiche <anis.megdiche@gmail.com>
2025-09-05 17:35:51 +0200

    refactor: clean up DemoAuth provider by removing eslint-disable comments and prefixing unused parameters

--- src/modules/auth/providers/DemoAuth.ts
+++ src/modules/auth/providers/DemoAuth.ts
@@ -11,26 +11,21 @@ import { TConfigUsers } from "../../core/types/TConfigUsers"
 
 //
 export class DemoAuth extends absAuthProvider {
-
-    // eslint-disable-next-line class-methods-use-this
+     
     Init(): void {
         Logger.Debug("DemoAuthProvider.Init")
     }
-
-    // eslint-disable-next-line class-methods-use-this
+     
     GetUsers() {
         return typia.random<TConfigUsers>()
     }
 
-    // eslint-disable-next-line class-methods-use-this, unused-imports/no-unused-vars
-    async Authenticate(userCredentials: TUserCredentials): Promise<TUserTokenInfo> {
+    async Authenticate(_userCredentials: TUserCredentials): Promise<TUserTokenInfo> {
         Logger.Debug("DemoAuthProvider.Authenticate")
         return typia.random<TUserTokenInfo>()
     }
 
-    // eslint-disable-next-line class-methods-use-this, unused-imports/no-unused-vars
-    async LogOut(username: string): Promise<void> {
+    async LogOut(_username: string): Promise<void> {
         Logger.Debug("DemoAuthProvider.LogOut")
     }
-
 }
