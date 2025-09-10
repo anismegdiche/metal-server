49c30094422a1051fd50afab19484afac109ee65
Anis Megdiche <anis.megdiche@gmail.com>
2025-09-10 11:44:38 +0200

    clean up

--- src/utils/JsonUtils/Stringify.ts
+++ src/utils/JsonUtils/Stringify.ts
@@ -1,15 +1,17 @@
 //
 //
 //
-//
-//
 import { configure } from 'safe-stable-stringify'
 
+
+//
 const SafeStableStringify = configure({
     circularValue: undefined,
     maximumDepth: 5
 })
 
+
+//
 export function Stringify<T>(json: T): string {
     try {
         return JSON.stringify(json)
