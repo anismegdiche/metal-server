0e613e0e55772c15acb8d57927e506f55d980d97
Anis Megdiche <anis.megdiche@gmail.com>
2026-01-13 16:10:46 +0100

    Add optional wait parameter to Queue for concurrent task execution

--- src/utils/Queue.ts
+++ src/utils/Queue.ts
@@ -12,25 +12,36 @@ type TQueueFunction = Function | void
 export class Queue {
 
     IsRunning: boolean
-    readonly Tasks: (TQueueFunction)[]
+    readonly Tasks: TQueueFunction[]
 
     constructor() {
         this.Tasks = []
         this.IsRunning = false
     }
 
-    async ProcessQueue(): Promise<void> {
+    async ProcessQueue(wait: boolean = true): Promise<void> {
         this.IsRunning = true
+        const promises: Promise<void>[] = [];
         while (this.IsRunning && this.Tasks.length > 0) {
-            const tasks = this.Tasks.splice(0)
-            await Promise.all(tasks.map(task => task && task()))
+            const task = this.Tasks.shift()
+            if (task)
+                if (wait)
+                    await task()
+                else
+                    promises.push(task())
         }
+        if (!wait)
+            await Promise.all(promises)
+
         this.IsRunning = false
     }
 
-    async Enqueue(task: TQueueFunction): Promise<void> {
+    async Enqueue(task: TQueueFunction, wait: boolean = true): Promise<void> {
+        if (!task)
+            return
+
         this.Tasks.push(task)
         if (!this.IsRunning)
-            this.ProcessQueue()
+            this.ProcessQueue(wait)
     }
 }
